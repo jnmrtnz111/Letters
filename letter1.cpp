411407a55e40e849345f4180039364a1ea467fe4
jnmrtnz111 <juamartinezh@unal.edu.co>
2024-08-03 07:12:02 -0500

    Add files via upload

---
+++ letter1.cpp
@@ -0,0 +1,49 @@
+/*
+Estudiante: Juan David M
+Profesores: Ever Rojas y Oscar Ramirez
+
+Documentación:https://photos.app.goo.gl/KSCkdgmug1DckFCy7
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    cout<<"Hola, profesores Ever y Oscar. Si están leyendo este código es porque mi tía fue una buena";
+    cout<<"paloma mensajera (10/10). Les traje algunos obsequios pequeños de un viaje que tuve a la";
+    cout<<"Universidad Nacional de Rosario (UNR) en Argentina para una competencia de programación en";
+    cout<<"la que estuve representando a la UNAL. Espero que sea de su agrado. No hay cosas materiales";
+    cout<<"que representen la infinita gratitud y admiración que les he tenido desde que me formaron";
+    cout<<"cuando era niño en la programación. Espero que el destino  (Dios o lo que sea) me siga permitiendo";
+    cout<<"participar en más competencias de programación.Si es así, empeicen a acostumbrarse :),";
+    cout<<"porque les estaré mandando pequeños obsequios de los viajes a los certámenes de programación";
+    cout<<"que el destino me permita vivir fuera del país.Gracias totales. (Lease hasta el final)";
+                                                                       |>>>
+                               _                      _                |
+                ____________ .' '.    _____/----/-\ .' './========\   / \
+               //// ////// /V_.-._\  |.-.-.|===| _ |-----| u    u |  /___\
+              // /// // ///==\ u |.  || | ||===||||| |T| |   ||   | .| u |_ _ _ _ _ _
+             ///////-\////====\==|:::::::::::::::::::::::::::::::::::|u u| U U U U U
+             |----/\u |--|++++|..|'''''''''''::::::::::::::''''''''''|+++|+-+-+-+-+-+
+             |u u|u | |u ||||||..|              '::::::::'           |===|>=== _ _ ==
+             |===|  |u|==|++++|==|              .::::::::.           | T |....| V |..
+             |u u|u | |u ||HH||         \|/    .::::::::::.
+             |===|_.|u|_.|+HH+|_              .::::::::::::.              _
+                            __(_)___         .::::::::::::::.         ___(_)__
+            ---------------/  / \  /|       .:::::;;;:::;;:::.       |\  / \  \-------
+            ______________/_______/ |      .::::::;;:::::;;:::.      | \_______\________
+            |       |     [===  =] /|     .:::::;;;::::::;;;:::.     |\ [==  = ]   |
+            |_______|_____[ = == ]/ |    .:::::;;;:::::::;;;::::.    | \[ ===  ]___|____
+                 |       |[  === ] /|   .:::::;;;::::::::;;;:::::.   |\ [=  ===] |
+            _____|_______|[== = =]/ |  .:::::;;;::::::::::;;;:::::.  | \[ ==  =]_|______
+             |       |    [ == = ] /| .::::::;;:::::::::::;;;::::::. |\ [== == ]      |
+            _|_______|____[=  == ]/ |.::::::;;:::::::::::::;;;::::::.| \[  === ]______|_
+               |       |  [ === =] /.::::::;;::::::::::::::;;;:::::::.\ [===  =]   |
+            ___|_______|__[ == ==]/.::::::;;;:::::::::::::::;;;:::::::.\[=  == ]___|_____
+
+    cout<<"Postdata: Discúlpeme profesor Oscar por no haberme comunicado con usted con la continuación del proyecto.";
+    cout<<"Lastimosamente fue un fracaso, todos los niños solo copiaban código de chatGPT y era muy desgastante para mí";
+    cout<<"preparar todo y ver que ellos no tenían el interés de aprender.";
+    cout<<"Juan David...";
+}
